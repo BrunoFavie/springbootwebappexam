7878b14df88d0c14af2cef0708e6ff563eecb9e2
Bruno Favié <bruno.favie@capgemini.com>
2017-07-17 12:38:22 +0200

    loading initial app.

---
+++ src/main/java/guru/springframework/SpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import guru.springframework.util.SpringBootStarter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+    public static void main(String[] args) {
+        new SpringBootStarter().StartWebApplication();
+    }
+}

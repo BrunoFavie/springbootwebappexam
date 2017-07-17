9b5534a33abab520130a996b035336af9ffdccff
Bruno Favié <bruno.favie@capgemini.com>
2017-07-17 15:25:07 +0200

    Added Delete API to controller.

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -19,28 +19,27 @@ public class ProductController {
         this.productService = productService;
     }
 
-//Get all products for website
+    //Get all products for website
     @RequestMapping(value = "/products", method = RequestMethod.GET)
-    public String list(Model model) throws OutOfMemoryError{
+    public String list(Model model) throws OutOfMemoryError {
         try {
             model.addAttribute("products", productService.listAllProducts());
             System.out.println("Returning products:");
-        }
-        catch(OutOfMemoryError e){
+        } catch (OutOfMemoryError e) {
             throw new OutOfMemoryError("Memory ran out while loading products");
         }
         return "products";
     }
 
     @RequestMapping("product/{id}")
-    public String showProduct(@PathVariable Integer id, Model model){
+    public String showProduct(@PathVariable Integer id, Model model) {
         model.addAttribute("product", productService.getProductById(id));
         return "productshow";
     }
 
     // editing product using webapplication
     @RequestMapping("product/edit/{id}")
-    public String edit(@PathVariable Integer id, Model model){
+    public String edit(@PathVariable Integer id, Model model) {
         model.addAttribute("product", productService.getProductById(id));
         return "productform";
     }
@@ -48,15 +47,14 @@ public class ProductController {
 
     //Adding new product using website
     @RequestMapping("product/new")
-    public String newProduct(Model model){
+    public String newProduct(Model model) {
         model.addAttribute("product", new Product());
         return "productform";
     }
 
 
-
     @RequestMapping(value = "product", method = RequestMethod.POST)
-    public String saveProduct(Product product){
+    public String saveProduct(Product product) {
 
         productService.saveProduct(product);
 
@@ -80,22 +78,22 @@ public class ProductController {
     //Adding new product using REST API for mobile application
     @ResponseBody
     @RequestMapping(value = "api/product/add", method = RequestMethod.POST)
-    public Boolean add(@RequestBody Product product){
+    public Boolean add(@RequestBody Product product) {
         Boolean success = false;
         Boolean exists = false;
         //check existing records
-        for (Product p:productService.listAllProducts()) {
-            if ( p.getId() == product.getId()){
+        for (Product p : productService.listAllProducts()) {
+            if (p.getId() == product.getId()) {
                 exists = true;
             }
         }
 
-        if(!exists){
+        if (!exists) {
             try {
                 productService.saveProduct(product);
                 success = true;
 
-            }catch(Exception e){
+            } catch (Exception e) {
             }
         }
         return success;
@@ -103,16 +101,29 @@ public class ProductController {
 
     // editing product using REST API for mobile application
     @ResponseBody
-    @RequestMapping("api/product/edit")
-    public Boolean edit(@RequestBody Product product){
+    @RequestMapping(value = "api/product/edit", method = RequestMethod.POST)
+    public Boolean edit(@RequestBody Product product) {
         Boolean success = false;
         try {
             productService.saveProduct(product);
             success = true;
 
-        }catch(Exception e){
+        } catch (Exception e) {
         }
         return success;
     }
 
-}
+    // Deleting product using REST API for mobile application
+    @ResponseBody
+    @RequestMapping(value = "api/product/delete", method = RequestMethod.DELETE)
+    public Boolean deleteProduct(@RequestParam Integer id) {
+        boolean deleted = false;
+        try {
+            Product p = productService.getProductById(id);
+            productService.deleteProduct(p);
+            deleted = true;
+        } catch (Exception e) {
+        }
+        return deleted;
+    }
+    }

5a5fc73b2a72bd0c1ec1fc6c4229e906b119c0f9
Bruno Favié <bruno.favie@capgemini.com>
2017-07-17 16:10:32 +0200

    Added guest pages to website

---
+++ src/main/java/guru/springframework/controllers/CustomerController.java
@@ -0,0 +1,16 @@
+package guru.springframework.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CustomerController {
+
+    @RequestMapping(value = "/customer", method = RequestMethod.GET)
+    public String list(Model model) {
+        return "customer";
+    }
+}
